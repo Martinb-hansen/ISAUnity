49277f2940d11bee69b67aa5ce57371d78cf0cd7
Martinb-hansen <bjerkov@gmail.com>
2022-03-07 19:52:45 +0100

    LorteLort

---
+++ PlaygroundCollider/Assets/Scripts/WorkspaceTransform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkspaceTransform : MonoBehaviour
+{
+
+    public float X;
+    public float Y;
+    public float Z;
+
+    public GameObject Workspace;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Workspace.transform.localScale = new Vector3(X, Y, Z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
